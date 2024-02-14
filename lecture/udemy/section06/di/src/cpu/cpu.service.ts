65d36c6afabb01a48937e20d709f8ffddd43f94b
Jake1152 <rancho974@gmail.com>
2024-02-14 20:51:55 +0900

    [ADD] 36. Consuming Multiple Modules
    - computer module과의 최종연결
    - /computer 라우터 동작 추가

--- lecture/udemy/section06/di/src/cpu/cpu.service.ts
+++ lecture/udemy/section06/di/src/cpu/cpu.service.ts
@@ -5,6 +5,10 @@ import { PowerService } from 'src/power/power.service';
 export class CpuService {
   // private로 두는 이유?
   constructor(private powerService: PowerService) {}
+  /**
+   * 10 watts 전력 사용
+   * 10번 전달 예정
+   */
   conpute(a: number, b: number) {
     console.log('Drawing 10 watss of power from Power Service');
     this.powerService.supplyPower(10);

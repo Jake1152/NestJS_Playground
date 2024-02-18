c04e338f0d43708ab353fef924470cb84a61bd1f
Jake1152 <rancho974@gmail.com>
2024-02-18 20:38:11 +0900

    [ADD] 51. Creating and Saving a User
    - Create User using HTTP POST Method

--- lecture/udemy/my-car-value/src/users/users.controller.ts
+++ lecture/udemy/my-car-value/src/users/users.controller.ts
@@ -1,12 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth') // 밑에 구현되는 메서드는 /auth/로 시작
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
-    console.log('body');
+    console.log('body ', body);
+    this.usersService.create(body.email, body.password);
   }
-  // DAO
+
+  // @Get('signup')
+  // getUserList() {
+  //   const usersList = this.usersService.find();
+  //   return usersList;
+  // }
 }
--- lecture/udemy/my-car-value/src/users/users.service.ts
+++ lecture/udemy/my-car-value/src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  /**
+   * !종속성 주입은 제네릭과 제대로 작동하지 않는다.
+   * @InjectRepository(User) 데코레이터는 "repo: Repository<User>"에서 제네릭 유형을 사용해야하기 때문에 필요하다고 한다.
+   */
+  //@InjectRepository() 를 user와 함께 호출
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    // 사용자 인스턴스 생성, 파라미터로 제공된 데이터를 객체에 추가
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+
+  // find() {
+  //   const usersList = this.repo.find();
+  //   return usersList;
+  // }
+}

3176f4653f8351f02f5048ef1d4447a925a61273
Jake1152 <rancho974@gmail.com>
2024-02-26 19:57:43 +0900

    [ADD] 65. Serialization in the Interceptor
    - 여전히 password 데이터는 걸러져서 나온다 serialize가 의도대로 잘
      동작하고 있다

--- lecture/udemy/my-car-value/src/interceptors/serialize.interceptor.ts
+++ lecture/udemy/my-car-value/src/interceptors/serialize.interceptor.ts
@@ -7,8 +7,8 @@ import {
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { classToPlain } from 'class-transformer';
-import { UserDto } from '../users/dtos/user.dto' }
+import { plainToClass } from 'class-transformer';
+import { UserDto } from '../users/dtos/user.dto';
 
 /**
  * 이해해야 할 정말 중요한 것은 요청이 처리되기 전에 무언가를 수행하려는 경우 해당 코드를
@@ -17,14 +17,11 @@ import { UserDto } from '../users/dtos/user.dto' }
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-    // Run something before a request is handled by the request handler
-    console.log('I am running before the handler', context);
-
-    //
     return handler.handle().pipe(
       map((data: any) => {
-        // Run something
-        console.log('I am running before response is sent out', data);
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }

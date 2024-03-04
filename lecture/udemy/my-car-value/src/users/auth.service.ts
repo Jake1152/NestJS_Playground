35280dcc3ba655d05a5b35467311bd00e468f4c6
Jake1152 <rancho974@gmail.com>
2024-03-04 18:31:38 +0900

    [ADD] 72. Implementing Signup Functionality
    - AuthService에 signup 메서드 세팅, 이미 있는 이메일로 회원가입 요청하는 경우 에러 처리

--- lecture/udemy/my-car-value/src/users/auth.service.ts
+++ lecture/udemy/my-car-value/src/users/auth.service.ts
@@ -1,10 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { UsersService } from './users.service';}
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { UsersService } from './users.service';
+
 /**
  * user service 복사본 피룡
  */
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
-    
+  constructor(private usersService: UsersService) {}
+
+  // 가입에 초점을 맞춤
+  async signup(email: string, password: string) {
+    // See if email is in use
+    const users = await this.usersService.find(email);
+    // 중복 유저가 있다는 의미, 에러발생
+    if (users.length) {
+      throw new BadRequestException('emial in use');
+    }
+
+    // Hash the users pwassword
+
+    // Create a new user and save it
+
+    // return the user
+  }
+
+  signin() {}
 }

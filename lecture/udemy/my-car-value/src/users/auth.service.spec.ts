9baa9eb83e2630587ada84b2dfa23429adf353f6
Jake1152 <rancho974@gmail.com>
2024-03-04 19:42:09 +0900

    [ADD] 93. Getting TypeScript to Help With Mocks

--- lecture/udemy/my-car-value/src/users/auth.service.spec.ts
+++ lecture/udemy/my-car-value/src/users/auth.service.spec.ts
@@ -4,11 +4,16 @@ import { UsersService } from './users.service';
 
 it('can create an instacne of auth service', async () => {
   // Create a fake copy of the users service
-  const fakeUsersService = {
-    find: () => Promise.resolve([]),
+  //   const fakeUsersService = {
+  //  Partial<UsersService> fake 메서드가 올바른 type을 가지고 있는지 확인해준다.
+  const fakeUsersService: Partial<UsersService> = {
+    // find: () => Promise.resolve([]),
+    find: () => Promise.resolve(1232132131),
     create: (email: string, password: string) =>
       Promise.resolve({ id: 1, email, password }),
   };
+
+  //   const arr = await fakseUsersService.find();
 
   const module = await Test.createTestingModule({
     providers: [

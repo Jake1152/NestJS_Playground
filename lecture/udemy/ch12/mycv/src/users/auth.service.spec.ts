2e1decbbfcc5275f80513c64aefc41f70d82e357
Jake1152 <rancho974@gmail.com>
2024-03-06 18:56:38 +0900

    [ADD] 98. Changing Mock Implementations
    - find() 메서드 정상 호출 여부 확인

--- lecture/udemy/ch12/mycv/src/users/auth.service.spec.ts
+++ lecture/udemy/ch12/mycv/src/users/auth.service.spec.ts
@@ -5,12 +5,14 @@ import { User } from './user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     // Create a fake copy of the users service
     //   const fakeUsersService = {
     //  Partial<UsersService> fake 메서드가 올바른 type을 가지고 있는지 확인해준다.
-    const fakeUsersService: Partial<UsersService> = {
+    // const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -37,9 +39,21 @@ describe('AuthService', () => {
     const user = await service.signup('asdfsdf@sdf.com', 'asdf');
     // hash화된 비번이 있어야한다
     expect(user.password).not.toEqual('asdfg');
-    const [salt, hash] = user.password.split('$');
+    const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  // 적어도 하나의 배열 리턴 확인
+  // done은 끝났는지 확인하는 용도의 파리미터
+  it("throws an error if user signs up with email that is in use", async (done) => {
+    fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1'} as User]);
+    try {
+      await service.signup('asdf@asdf.com', 'asdf');
+
+    } catch (err) {
+      // done();
+    }
   });
 
 });

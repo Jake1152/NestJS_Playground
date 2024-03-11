9765de68a13ef5e4bd30a5dd1ccffe117ac9a841
Jake1152 <rancho974@gmail.com>
2024-03-11 19:19:29 +0900

    [ADD] second way to fix validation pipe

--- lecture/udemy/ch13/mycv/src/main.ts
+++ lecture/udemy/ch13/mycv/src/main.ts
@@ -1,28 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-// import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-// const cookieSession = require('cookie-session');
-import { setupApp } from './setup-app';
+const cookieSession = require('cookie-session');
+// import { setupApp } from './setup-app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  setupApp(app);
-  // app.use(
-  //   cookieSession({
-  //     keys: ['asdfasfd'],
-  //   }),
-  // );
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //   }),
-  // );
-  // (app as any).set('etag', false);
-  // app.use((req, res, next) => {
-  //   res.removeHeader('x-powered-by');
-  //   res.removeHeader('date');
-  //   next();
-  // });
+  // setupApp(app);
+  app.use(
+    cookieSession({
+      keys: ['asdfasfd'],
+    }),
+  );
+  // -> to be in app.modules // validation pipe
+  app.useGlobalPipes();
+  (app as any).set('etag', false);
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
